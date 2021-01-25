bcdbf4f2a7d79395d2613f00272577cdca61cf8f
Glatzer, Thomas <glatzert@uni-mainz.de>
2021-01-25 13:50:20 +0100

    throw catch in test-programm

--- src/HIBP-Policy/main.cpp
+++ src/HIBP-Policy/main.cpp
@@ -7,10 +7,16 @@ int main(int argc, const char* argv[]) {
 
     HIBPPolicyEvaluator evaluator = HIBPPolicyEvaluator();
 
-    std::wstring password = std::wstring(L"Password123.");
-    bool allowPasswordChange = evaluator.IsAcceptablePassword(password);
+    try {
+        std::wstring password = std::wstring(L"Password123.");
+        bool allowPasswordChange = evaluator.IsAcceptablePassword(password);
 
-    wprintf((password + L" => %d").c_str(), allowPasswordChange);
+        wprintf((password + L" => %d").c_str(), allowPasswordChange);
+    }
+    catch (std::exception e) {
+        wprintf(L"Exception occured:\n");
+        printf(e.what());
+    }
 };
 
 #endif
